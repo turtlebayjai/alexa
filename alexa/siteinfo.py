12832ea5b3ee3cbebde864ee4f7ef6d533353ebb
Jai Lakhanpal <jlak212@gmail.com>
2020-09-19 14:45:55 -0400

    siteinfo/get_similar_sites

--- alexa/siteinfo.py
+++ alexa/siteinfo.py
@@ -14,9 +14,33 @@ def get_competitors(website):
     Returns:
         list of strings, or None: competitor websites.
     """
-    formatted = helpers.format_website_string(website)
-    url = f"{URL}/{formatted}"
-    response = helpers.get_response(url)
+    response = _get_siteinfo(website)
     sel = scrapy.Selector(text=response.text)
     sites = eval(sel.css("script#competitorsJSON::text").extract_first())
     return sites.get("competitors")
+
+
+def get_similar_sites(website):
+    """Returns a list of similar sites for a given website.
+    Args:
+        website (string): example - "mysite.com"
+    Returns:
+        dict (string : float): similar websites and corresponding overlap score.
+    """
+    response = _get_siteinfo(website)
+    sel = scrapy.Selector(text=response.text)
+    sites = sel.css(
+        "div#card_mini_audience div.Body > div.Row > div.site > a::text"
+    ).extract()
+    overlaps = sel.css(
+        "div#card_mini_audience div.Body > div.Row > div.overlap > span.truncation::text"
+    ).extract()
+    similar_sites = {site.strip(): overlap for site, overlap in zip(sites, overlaps)}
+    return similar_sites
+
+
+def _get_siteinfo(website):
+    """Returns response object after properly formatting website string."""
+    formatted = helpers.format_website_string(website)
+    url = f"{URL}/{formatted}"
+    return helpers.get_response(url)

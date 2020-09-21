2d2d542a1884fa886ddb105ae037bfc86fb06b74
Jai Lakhanpal <jlak212@gmail.com>
2020-09-21 19:34:42 -0400

    TestGetSimilarSites

--- tests/test_siteinfo.py
+++ tests/test_siteinfo.py
@@ -20,9 +20,36 @@ class TestGetCompetitors(unittest.TestCase):
         self.assertTrue(len(competitors) > 0)
 
     def test_invalid_website(self):
+        website = "https://www.badinput.com"
+        competitors = siteinfo.get_competitors(website)
+        self.assertEqual(competitors, None)
+
+    def test_invalid_input(self):
         website = "https://www.badinput.badinput"
         competitors = siteinfo.get_competitors(website)
         self.assertEqual(competitors, None)
+
+
+class TestGetSimilarSites(unittest.TestCase):
+    def test_nice_input(self):
+        website = "google.com"
+        similar = siteinfo.get_similar_sites(website)
+        self.assertTrue(len(similar) > 0)
+
+    def test_unformatted_input(self):
+        website = "htTPs://WWW.GoOgLe.COM"
+        similar = siteinfo.get_similar_sites(website)
+        self.assertTrue(len(similar) > 0)
+
+    def test_invalid_website(self):
+        website = "https://www.badinput.com"
+        similar = siteinfo.get_similar_sites(website)
+        self.assertEqual(similar, None)
+
+    def test_invalid_input(self):
+        website = "https://www.badinput.badinput"
+        similar = siteinfo.get_similar_sites(website)
+        self.assertEqual(similar, None)
 
 
 if __name__ == "__main__":

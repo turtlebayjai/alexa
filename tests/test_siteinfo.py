c84c7253472364619073c27c87f591380f98f3c4
Jai Lakhanpal <jlak212@gmail.com>
2020-09-22 19:55:31 -0400

    refactor

--- tests/test_siteinfo.py
+++ tests/test_siteinfo.py
@@ -7,49 +7,57 @@ from context import siteinfo
 
 logging.basicConfig(level=logging.INFO)
 
+INPUTS = {
+    "good": "google.com",
+    "unformatted": "htTPs://WWW.GoOgLe.COM",
+    "bad": "https://www.badinput.com",
+    "invalid": "invalidinput.invalidinput",
+}
+
+FUNCS = {
+    "get_competitors": siteinfo.get_competitors,
+    "get_similar_sites": siteinfo.get_similar_sites,
+}
+
 
 class TestGetCompetitors(unittest.TestCase):
-    def test_nice_input(self):
-        website = "google.com"
-        competitors = siteinfo.get_competitors(website)
-        self.assertTrue(len(competitors) > 0)
+    test = "get_competitors"
+
+    def test_good_input(self):
+        output = FUNCS[self.test](INPUTS["good"])
+        self.assertTrue(len(output) > 0)
 
     def test_unformatted_input(self):
-        website = "htTPs://WWW.GoOgLe.COM"
-        competitors = siteinfo.get_competitors(website)
-        self.assertTrue(len(competitors) > 0)
+        output = FUNCS[self.test](INPUTS["unformatted"])
+        self.assertTrue(len(output) > 0)
 
-    def test_invalid_website(self):
-        website = "https://www.badinput.com"
-        competitors = siteinfo.get_competitors(website)
-        self.assertEqual(competitors, None)
+    def test_bad_input(self):
+        output = FUNCS[self.test](INPUTS["bad"])
+        self.assertEqual(output, None)
 
     def test_invalid_input(self):
-        website = "https://www.badinput.badinput"
-        competitors = siteinfo.get_competitors(website)
-        self.assertEqual(competitors, None)
+        output = FUNCS[self.test](INPUTS["invalid"])
+        self.assertEqual(output, None)
 
 
 class TestGetSimilarSites(unittest.TestCase):
-    def test_nice_input(self):
-        website = "google.com"
-        similar = siteinfo.get_similar_sites(website)
-        self.assertTrue(len(similar) > 0)
+    test = "get_similar_sites"
+
+    def test_good_input(self):
+        output = FUNCS[self.test](INPUTS["good"])
+        self.assertTrue(len(output) > 0)
 
     def test_unformatted_input(self):
-        website = "htTPs://WWW.GoOgLe.COM"
-        similar = siteinfo.get_similar_sites(website)
-        self.assertTrue(len(similar) > 0)
+        output = FUNCS[self.test](INPUTS["unformatted"])
+        self.assertTrue(len(output) > 0)
 
-    def test_invalid_website(self):
-        website = "https://www.badinput.com"
-        similar = siteinfo.get_similar_sites(website)
-        self.assertEqual(similar, None)
+    def test_bad_input(self):
+        output = FUNCS[self.test](INPUTS["bad"])
+        self.assertEqual(output, None)
 
     def test_invalid_input(self):
-        website = "https://www.badinput.badinput"
-        similar = siteinfo.get_similar_sites(website)
-        self.assertEqual(similar, None)
+        output = FUNCS[self.test](INPUTS["invalid"])
+        self.assertEqual(output, None)
 
 
 if __name__ == "__main__":
